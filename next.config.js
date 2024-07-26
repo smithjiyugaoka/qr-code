cc2f90703594207f24c5f1814a9e0ef5b2269467
smithjiyugaoka <smith@apexes.co.jp>
2024-07-26 07:22:33 +0000

    Your commit message here

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    swcMinify: true,
-      webpack: (config) => {
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@': path.resolve(__dirname, 'src'),
-  },
-}
+        const path = require('path');
 
-module.exports = nextConfig
+        /** @type {import('next').NextConfig} */
+        const nextConfig = {
+          reactStrictMode: true,
+          swcMinify: true,
+          webpack: (config) => {
+            config.resolve.alias = {
+              ...config.resolve.alias,
+              '@': path.resolve(__dirname, 'src'),
+            };
+            return config;
+          },
+        };
+
+        module.exports = nextConfig;
